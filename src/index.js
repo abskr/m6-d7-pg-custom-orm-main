699a128f528b60ed36d0d7371de500f776594ea9
ABskr <bskr26@gmx.de>
2021-05-04 16:36:38 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const services = require("./services");
+const listEndpoints = require("express-list-endpoints");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api", services);
+
+console.log(listEndpoints(app))
+app.listen(port, () => console.info(" 🚀 Server is running: ", port));
+
+app.on("error", (error) =>
+  console.info(" ❌ Server is not running due to : ", error)
+);
